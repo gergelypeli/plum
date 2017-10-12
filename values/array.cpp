806f935ab5591d86f3056506e9a66714442484f2
Gergely Peli <gergely.peli@ply.hu>
2017-10-12 02:38:00 +0200

    String comparison by contents.

--- values/array.cpp
+++ values/array.cpp
@@ -319,11 +319,51 @@ public:
     }
 
     virtual Regs precompile(Regs preferred) {
-        return left->precompile(preferred) | right->precompile(preferred);
+        Regs clob = left->precompile(preferred) | right->precompile(preferred);
+        clob.add(RAX).add(RBX).add(RCX).add(RSI).add(RDI);
+        return clob;
     }
 
     virtual Storage compile(X64 *x64) {
-        compile_and_store_both(x64, Storage(), Storage());
-        return Storage(CONSTANT, 1);
+        Label strcmp_label = x64->once(compile_strcmp);
+
+        compile_and_store_both(x64, Storage(STACK), Storage(STACK));
+        
+        x64->op(POPQ, RBX);
+        x64->op(POPQ, RAX);
+        x64->op(CALL, strcmp_label);
+        
+        return Storage(REGISTER, CL);
+    }
+    
+    static void compile_strcmp(X64 *x64) {
+        Label sete, done;
+        
+        x64->op(MOVB, CL, 0);
+        x64->op(CMPQ, RAX, RBX);
+        x64->op(JE, sete);
+        
+        x64->op(CMPQ, RAX, 0);
+        x64->op(JE, done);
+        x64->op(CMPQ, RBX, 0);
+        x64->op(JE, done);
+        
+        x64->op(MOVQ, RCX, x64->array_length_address(RAX));
+        x64->op(CMPQ, RCX, x64->array_length_address(RBX));
+        x64->op(JNE, sete);
+        
+        x64->op(LEA, RSI, x64->array_items_address(RAX));
+        x64->op(LEA, RDI, x64->array_items_address(RBX));
+        x64->op(REPECMPSW);
+        x64->op(CMPQ, RCX, 0);
+        
+        x64->code_label(sete);
+        x64->op(SETE, CL);
+        
+        x64->code_label(done);
+        x64->decref(RBX);
+        x64->decref(RAX);
+        
+        x64->op(RET);
     }
 };
