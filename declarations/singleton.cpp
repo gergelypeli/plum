4db564b1488ac09cdb0a083dcb757f6eb02c7cd8
Gergely Peli <gergely.peli@ply.hu>
2018-08-22 23:36:29 +0200

    Fixed SingletonType duplicating the prefix matching of Type.
    Removed unused code from SingletonType.
    
    Removed unused members from ModuleType.

--- declarations/singleton.cpp
+++ declarations/singleton.cpp
@@ -2,19 +2,14 @@
 class SingletonType: public Type {
 public:
     std::vector<Allocable *> member_allocables;
-    //std::vector<TypeSpec> member_tss;  // rvalues, for the initializer arguments
     std::vector<std::string> member_names;
     Function *initializer_function;
     Function *finalizer_function;
-    std::string prefix;
-    //BaseRole *base_role;
 
     SingletonType(std::string name)
         :Type(name, {}, singleton_metatype) {
         initializer_function = NULL;
         finalizer_function = NULL;
-        prefix = name + ".";
-        //base_role = NULL;
     }
 
     virtual Value *matched(TypeSpec result_ts) {
@@ -27,7 +22,6 @@ public:
             
             if (v) {
                 member_allocables.push_back(v);
-                //member_tss.push_back(v->alloc_ts.rvalue());
                 member_names.push_back(v->name);
             }
             
@@ -50,49 +44,11 @@ public:
                     
                 finalizer_function = f;
             }
-            
-            //BaseRole *b = ptr_cast<BaseRole>(c.get());
-            
-            //if (b) {
-            //    if (base_role) {
-            //        std::cerr << "Multiple base roles!\n";
-            //        return false;
-            //    }
-                
-            //    base_role = b;
-            //}
         }
         
         std::cerr << "Singleton " << name << " has " << member_allocables.size() << " member variables.\n";
         return true;
     }
-
-    virtual void allocate() {
-        // Let the base be allocated first, then it will skip itself
-        
-        //if (base_role) {
-        //    base_role->allocate();
-        //    inner_scope->set_virtual_entry(0, this);
-        //}
-        //else {
-        //    std::vector<VirtualEntry *> vt = { this };
-        //    inner_scope->virtual_reserve(vt);
-        //}
-            
-        Type::allocate();
-    }
-    /*
-    virtual Label get_virtual_entry_label(TypeMatch tm, X64 *x64) {
-        // This must point to our base type's virtual table, or NULL
-        
-        if (base_role)
-            return typesubst(base_role->alloc_ts, tm).get_virtual_table_label(x64);
-        else {
-            //std::cerr << "Class " << tm << " has no base.\n";
-            return x64->runtime->zero_label;
-        }
-    }
-    */
 
     virtual Allocation measure(TypeMatch tm) {
         return inner_scope->get_size(tm);
@@ -146,31 +102,12 @@ public:
 
         inner_scope->set_meta_scope(singleton_metatype->get_inner_scope());
 
-        //Allocation vt_offset = is->reserve(Allocation(CLASS_HEADER_SIZE));  // VT pointer
-        //if (vt_offset.bytes != CLASS_VT_OFFSET)  // sanity check
-        //    throw INTERNAL_ERROR;
-        
         return inner_scope;
     }
 
-    //virtual std::vector<TypeSpec> get_member_tss(TypeMatch &match) {
-    //    std::vector<TypeSpec> tss;
-    //    for (auto &ts : member_tss)
-    //        tss.push_back(typesubst(ts, match));
-    //    return tss;
-    //}
-    
     virtual std::vector<std::string> get_member_names() {
         return member_names;
     }
-
-    //virtual std::vector<VirtualEntry *> get_virtual_table(TypeMatch tm) {
-    //    return inner_scope->get_virtual_table();
-    //}
-
-    //virtual Label get_virtual_table_label(TypeMatch tm, X64 *x64) {
-    //    return x64->once->compile(compile_virtual_table, tm[0]);
-    //}
 
     virtual Function *get_initializer_function() {
         return initializer_function;
@@ -197,14 +134,11 @@ public:
 class ModuleType: public Type {
 public:
     ModuleScope *module_scope;
-    std::vector<std::string> member_names;
-    Function *initializer_function;
     std::vector<SingletonType *> singleton_types;
 
     ModuleType(std::string name, ModuleScope *ms)
         :Type(name, {}, module_metatype) {
         module_scope = ms;
-        initializer_function = NULL;
     }
     
     virtual void store(TypeMatch tm, Storage s, Storage t, X64 *x64) {
@@ -220,39 +154,16 @@ public:
         
     virtual bool complete_type() {
         for (auto &c : module_scope->contents) {
-            Allocable *v = ptr_cast<Allocable>(c.get());
-            
-            if (v) {
-                //member_allocables.push_back(v);
-                //member_tss.push_back(v->alloc_ts.rvalue());
-                member_names.push_back(v->name);
-            }
-
-            Function *f = ptr_cast<Function>(c.get());
-            
-            if (f && f->type == INITIALIZER_FUNCTION) {
-                if (initializer_function) {
-                    std::cerr << "Multiple module initializers!\n";
-                    return false;
-                }
-                    
-                initializer_function = f;
-            }
-            
             SingletonType *s = ptr_cast<SingletonType>(c.get());
             
             if (s)
                 singleton_types.push_back(s);
         }
         
-        std::cerr << "Module " << name << " has " << member_names.size() << " member variables.\n";
+        std::cerr << "Module " << name << " has " << singleton_types.size() << " singletons.\n";
         return true;
     }
 
-    virtual std::vector<std::string> get_member_names() {
-        return member_names;
-    }
-    
     virtual void collect_initializer_labels(std::vector<Label> &labels, X64 *x64) {
         for (SingletonType *s : singleton_types) {
             Function *f = s->get_initializer_function();
